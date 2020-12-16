4d9e6659831e9429e24c928e2b0aaa6b8af4fe62
Dave <dub.cyclone@gmail.com>
2020-12-16 20:15:01 +0000

    Added ignore for build files. Added web project for Html. Nuget for Citizen.

--- VORP-Inputs[Client-Server]/vorpinputs_cl/vorpinputs_cl.cs
+++ VORP-Inputs[Client-Server]/vorpinputs_cl/vorpinputs_cl.cs
@@ -1,10 +1,7 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
 using System;
-using System.Collections.Generic;
 using System.Dynamic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace vorpinputs_cl
